511624a6a7b0db593dc10a09aa4715397fd52565
Luiz Eduardo Pereira <luizedu_pereira000@hotmail.com>
2019-08-15 15:17:10 -0300

    Grafico

--- dependencies/install_packages.sh
+++ dependencies/install_packages.sh
@@ -7,4 +7,15 @@
 
 # Multi-objective optimization utilities
 # https://esa.github.io/pagmo2/docs/python/utils/py_mo_utils.html
+echo 'Installing pygmo'
 pip3 install pygmo
+
+echo ''
+
+echo 'Installing python3-dev'
+sudo apt-get install python3-dev
+
+echo ''
+
+echo 'Installing matplotlib'
+pip3 install matplotlib
